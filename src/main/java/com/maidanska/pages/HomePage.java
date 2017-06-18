0a1f03ef3cb68acc0d2b12d8eeca8564d635f255
Anastasiia Maidanska <maidanska.a.e@gmail.com>
2017-06-18 22:10:00 +0300

    Change url and build configuration

--- src/main/java/com/maidanska/pages/HomePage.java
+++ src/main/java/com/maidanska/pages/HomePage.java
@@ -3,7 +3,7 @@ package com.maidanska.pages;
 import com.maidanska.utils.DriverHolder;
 
 public class HomePage extends BasePage {
-    private static final String HOME_URL = "https://ia.ca";
+    private static final String HOME_URL = "https://ia.ca/individuals";
 
     public HomePage() {
         DriverHolder.getDriver().get(HOME_URL);
